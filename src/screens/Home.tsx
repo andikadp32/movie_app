2f0dc64a9ebb2cb029049b9996f55d00e07bf87f
andikadp32 <andikadp32@gmail.com>
2024-06-26 21:30:25 +0700

    set up home screen navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-export default function Home(): JSX.Element {
+export default function Home({ navigation }: any): JSX.Element {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Movie Page</Text>
+      <Button
+        title="PERGI KE MOVIE DETAIL"
+        onPress={() => navigation.navigate('MovieDetail')}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 20
+  }
+})
